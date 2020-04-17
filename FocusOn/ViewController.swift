1a1339d44b9f5bde61f5645113f282199c2cc514
Scott Bolin <sbolin@gmail.com>
2020-04-17 14:49:24 +0700

    Initial Commit

---
+++ FocusOn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FocusOn
+//
+//  Created by Scott Bolin on 4/17/20.
+//  Copyright © 2020 Scott Bolin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
